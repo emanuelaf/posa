50190a9470a144835bc8ee39faa288c0f3540754
furfaro <furfaro@campus-035-205.ucdavis.edu>
2021-07-21 16:45:06 -0700

    minor changes

--- toy-example.R
+++ toy-example.R
@@ -1,5 +1,5 @@
 # Toy example
-source('codice/functions.R')
+source('code/functions.R')
 N = 10
 pi_init = rep(0.2, N)
 y = c(0,1,0,1,0,0,0,1,1,0)
@@ -17,6 +17,9 @@ for (i in 1:100000) {
    posa_ht_est[i] = t_posa_ht(s.pi_i = pi_i[s==1], s.y = y[s==1])/length(y)
    posa_pht_est[i] = t_posa_pht(s = s, y.s = y[s==1], pik = pi_init, cond=cond)/length(y)
 }
+
+# Empirical proof of unbiased estimators and correct equations for
+# pht and ht variances
 (mean(posa_ht_est)-sum(y)/length(y))/(sum(y)/length(y))*100
 (mean(posa_pht_est)-sum(y)/length(y))/(sum(y)/length(y))*100
 (var_pht_posa-var(posa_pht_est))/(var(posa_pht_est))*100
